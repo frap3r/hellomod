a55349671f195db5ea1ba18704db91eb8e629d3b
frap3r <88819348+frap3r@users.noreply.github.com>
2021-09-29 08:00:02 +0300

    Create hellomod.go

---
+++ hellomod.go
@@ -0,0 +1,18 @@
+package hellomod
+
+import "fmt"
+
+// English
+func Eng(){
+  fmt.Println("Hello !")
+}
+
+// French
+func Fra(){
+  fmt.Println("Bonjour !")
+}
+
+// Italian
+func Ita(){
+  fmt.Println("Ciao !")
+}
